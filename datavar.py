d866c0085dd607b00e1fb6d9b16d2f4c4184d501
sahil <sahil@gmail.com>
2022-04-27 23:41:01 +0530

    Initial commit

---
+++ datavar.py
@@ -0,0 +1,18 @@
+a = 2
+b = 4
+sum =a + b
+print(sum)
+
+area = 10
+print(type(area))
+area = "10"
+print(type(area))
+
+name = "sam"
+print(name[2])
+
+#slicing
+
+sahil = "abcdef"
+print(sahil[0:3])
+print(sahil[1:4])
